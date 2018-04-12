7ebea1318b5d8ea70715bc014b03063865f16194
Apal Shah <apalshah21121996@gmail.com>
2018-04-12 21:20:53 +0530

    added icons, fonts and changes in spacing

--- app/src/main/java/co/techtiques/techtiques/MainActivity.kt
+++ app/src/main/java/co/techtiques/techtiques/MainActivity.kt
@@ -3,6 +3,7 @@ package co.techtiques.techtiques
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
