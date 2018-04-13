29ef2d08981f389c44650a0a006f94601477373d
Apal Shah <apalshah21121996@gmail.com>
2018-04-13 16:05:25 +0530

    bottom navigation

--- app/src/main/java/co/techtiques/techtiques/MainActivity.kt
+++ app/src/main/java/co/techtiques/techtiques/MainActivity.kt
@@ -2,6 +2,13 @@ package co.techtiques.techtiques
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.support.annotation.NonNull
+import android.support.design.widget.BottomNavigationView
+import android.view.MenuItem
+import co.techtiques.techtiques.R.id.*
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.toast
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +16,22 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigation()
+    }
+
+    fun bottomNavigation() {
+        bottom_navigation.setSelectedItemId(action_home)
+        bottom_navigation.setOnNavigationItemSelectedListener(
+            object : BottomNavigationView.OnNavigationItemSelectedListener {
+                override fun onNavigationItemSelected(item: MenuItem): Boolean {
+                    when (item.getItemId()) {
+                        action_settings -> toast("Settings")
+                        action_home -> toast("Home")
+                        action_profile -> toast("Profile")
+                    }
+                    return true
+                }
+            }
+        )
     }
 }
